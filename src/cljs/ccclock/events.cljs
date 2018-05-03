821afffd0cb13207019b2cd08a91b91f4bf293a7
Chad Stovern <chad@stovern.me>
2018-05-03 10:32:43 -0500

    updating app to use darksky's api for vastly improved local data
    - big refactor due to api differences
    - one api call down from two is also a nice side benefit
    - http call is still broken, need to troubleshoot

--- src/cljs/ccclock/events.cljs
+++ src/cljs/ccclock/events.cljs
@@ -12,34 +12,17 @@
 (defonce do-time (js/setInterval dispatch-event-update-time 1000))
 
 
-(defn fetch-weather []
-  (let [appid (get config/secrets :openweathermap-appid)
-        zip   (str (get config/secrets :postal-code) "," (get config/secrets :country-code))]
+(defn dispatch-event-update-weather []
+  (let [apikey      (get config/secrets :darksky-apikey)
+        coords      (get config/secrets :lat-lon-coords)
+        request-url (str "https://api.darksky.net/forecast/" apikey "/" coords)]
     (GET
-     "https://api.openweathermap.org/data/2.5/weather"
-     {:params          {"zip"   zip
-                        "units" "imperial"
-                        "appid" appid}
-      :handler         #(re-frame/dispatch [::update-weather %1])
+     request-url
+     {:handler         #(re-frame/dispatch [::update-weather %1])
       :response-format :json
       :keywords?       true})))
-(fetch-weather) ; run once on load to populate state db
-(defonce do-weather (js/setInterval fetch-weather 300000))
-
-
-(defn fetch-forecast []
-  (let [appid (get config/secrets :openweathermap-appid)
-        zip   (str (get config/secrets :postal-code) "," (get config/secrets :country-code))]
-    (GET
-     "https://api.openweathermap.org/data/2.5/forecast"
-     {:params          {"zip"   zip
-                        "units" "imperial"
-                        "appid" appid}
-      :handler         #(re-frame/dispatch [::update-forecast %1])
-      :response-format :json
-      :keywords?       true})))
-(fetch-forecast) ; run once on load to populate state db
-(defonce do-forecast (js/setInterval fetch-forecast 300000))
+(dispatch-event-update-weather) ; run once on load to populate state db
+(defonce do-weather (js/setInterval dispatch-event-update-weather 300000))
 
 
 ;; event handling
@@ -61,11 +44,3 @@
    [db [_ response]]
    (-> db
        (assoc :weather response))))
-
-
-(re-frame/reg-event-db
- ::update-forecast
- (fn
-   [db [_ response]]
-   (-> db
-       (assoc :forecast response))))
