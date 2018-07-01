4545052ec2e2df3c1a487c96856761f1029f9a3b
Chad Stovern <chad@stovern.me>
2018-07-01 17:05:26 -0500

    broke out clojure backend into core, route, handler namespaces

--- src/clj/ccclock/core.clj
+++ src/clj/ccclock/core.clj
@@ -1,45 +1,20 @@
 (ns ccclock.core
-  (:require [compojure.core           :refer [defroutes GET]]
-            [compojure.route          :as    route]
-            [ring.util.response       :as    response]
-            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
+  (:require [ccclock.route            :as    route])
+  (:require [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
             [ring.middleware.reload   :refer [wrap-reload]]
             [environ.core             :as    environ]
-            [ring.adapter.jetty       :refer [run-jetty]]
-            [clj-http.client          :as    client])
+            [ring.adapter.jetty       :refer [run-jetty]])
   (:gen-class))
 
 
-(defroutes app-routes
-  ;; NOTE: this will deliver all of your assets from the public directory
-  ;; of resources i.e. resources/public
-  (route/resources "/" {:root "public"})
-  ;; NOTE: this will deliver your index.html
-  (GET "/"        [] (-> (response/resource-response "index.html" {:root "public"})
-                         (response/content-type "text/html")))
-  (GET "/weather" [] (let [weather-url    "https://api.darksky.net/forecast"
-                           weather-apikey (environ/env :weather-apikey)
-                           lat-lon        (environ/env :lat-lon)
-                           result         (client/get
-                                           (str weather-url "/"
-                                                weather-apikey "/"
-                                                lat-lon))]
-                       {:status  200
-                        :headers {"Access-Control-Allow-Origin" "*"
-                                  "Access-Control-Allow-Headers" "Content-Type"
-                                  "Content-Type" "application/json"}
-                        :body    (get result :body)}))
-  (route/not-found "Page not found"))
-
-
 (def dev-app
-  (-> #'app-routes
+  (-> #'route/app-routes
       (wrap-defaults site-defaults)
       wrap-reload))
 
 
 (def app
-  (-> app-routes
+  (-> route/app-routes
       (wrap-defaults site-defaults)))
 
 
